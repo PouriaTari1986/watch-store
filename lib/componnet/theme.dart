0e08962d7a702d6d85096f91434b914373fa9823
PouriaTari1986 <pouria.developer86@gmail.com>
2025-08-15 22:12:41 +0330

    colors

---
+++ lib/componnet/theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color title = Colors.black;
+
+  static const Color hint = Color.fromARGB(255, 217, 220, 228);
+
+  static const Color appBar = Color.fromARGB(255, 255, 255, 255);
+
+  static const Color border = Color.fromARGB(255, 217, 217, 228);
+
+  static const Color borderSelected = Color.fromARGB(255, 0, 117, 251);
+
+  static const Color buttonColor = Color.fromARGB(255, 0, 117, 251);
+
+  static const Color surfaceColor = Color.fromARGB(255, 243, 243, 243);
+
+  static const Color iconColor = Color.fromARGB(255, 217, 220, 228);
+
+  static const Color iconSelected = Color.fromARGB(255, 0, 0, 0);
+
+  static const Color discountColor = Color.fromARGB(255, 255, 58, 58);
+
+  static const Color progressShpping = Color.fromARGB(255, 30, 130, 255);
+
+  static const Color continueButton = Color.fromARGB(255, 255, 97, 97);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,25 @@
 
 
 
+import 'package:flutter/material.dart';
+
 void main() {
 
+runApp(MyApp());
 
     
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   //root widget of platform
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "watch store",
+        home: Container(),
+    );
+  }
 }
 
 
