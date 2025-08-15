b6dae073ccd204b75e16657f15a5c82396b9c972
PouriaTari1986 <pouria.developer86@gmail.com>
2025-08-15 21:48:08 +0330

    assets

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -9,50 +9,145 @@
 // ignore_for_file: type=lint
 // ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
 
-class $AssetsIconsGen {
-  const $AssetsIconsGen();
+import 'package:flutter/widgets.dart';
 
-  /// File path: assets/icons/arrow left.svg
-  String get arrowLeft => 'assets/icons/arrow left.svg';
+class $AssetsPngGen {
+  const $AssetsPngGen();
 
-  /// File path: assets/icons/home-hashtag.svg
-  String get homeHashtag => 'assets/icons/home-hashtag.svg';
+  /// File path: assets/png/user.png
+  AssetGenImage get user => const AssetGenImage('assets/png/user.png');
 
-  /// File path: assets/icons/list.svg
-  String get list => 'assets/icons/list.svg';
+  /// File path: assets/png/whatch store.png
+  AssetGenImage get whatchStore =>
+      const AssetGenImage('assets/png/whatch store.png');
 
-  /// File path: assets/icons/shopping bascket.svg
-  String get shoppingBascket => 'assets/icons/shopping bascket.svg';
+  /// List of all assets
+  List<AssetGenImage> get values => [user, whatchStore];
+}
 
-  /// File path: assets/icons/user.svg
-  String get user => 'assets/icons/user.svg';
+class $AssetsSvgGen {
+  const $AssetsSvgGen();
+
+  /// File path: assets/svg/arrow left.svg
+  String get arrowLeft => 'assets/svg/arrow left.svg';
+
+  /// File path: assets/svg/home-hashtag.svg
+  String get homeHashtag => 'assets/svg/home-hashtag.svg';
+
+  /// File path: assets/svg/list.svg
+  String get list => 'assets/svg/list.svg';
+
+  /// File path: assets/svg/search-normal.svg
+  String get searchNormal => 'assets/svg/search-normal.svg';
+
+  /// File path: assets/svg/shopping bascket.svg
+  String get shoppingBascket => 'assets/svg/shopping bascket.svg';
+
+  /// File path: assets/svg/user.svg
+  String get user => 'assets/svg/user.svg';
 
   /// List of all assets
   List<String> get values => [
     arrowLeft,
     homeHashtag,
     list,
+    searchNormal,
     shoppingBascket,
     user,
   ];
 }
 
-class $AssetsImagesGen {
-  const $AssetsImagesGen();
-
-  /// File path: assets/images/user pic.svg
-  String get userPic => 'assets/images/user pic.svg';
-
-  /// File path: assets/images/whatch store.svg
-  String get whatchStore => 'assets/images/whatch store.svg';
-
-  /// List of all assets
-  List<String> get values => [userPic, whatchStore];
-}
-
 class Assets {
   const Assets._();
 
-  static const $AssetsIconsGen icons = $AssetsIconsGen();
-  static const $AssetsImagesGen images = $AssetsImagesGen();
+  static const $AssetsPngGen png = $AssetsPngGen();
+  static const $AssetsSvgGen svg = $AssetsSvgGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+    this.animation,
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+  final AssetGenImageAnimation? animation;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({AssetBundle? bundle, String? package}) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class AssetGenImageAnimation {
+  const AssetGenImageAnimation({
+    required this.isAnimation,
+    required this.duration,
+    required this.frames,
+  });
+
+  final bool isAnimation;
+  final Duration duration;
+  final int frames;
 }
