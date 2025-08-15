3518c6726dbae22de2aa3cad29c0a7a42a61cee1
PouriaTari1986 <pouria.developer86@gmail.com>
2025-08-15 21:41:41 +0330

    first commit

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,58 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/arrow left.svg
+  String get arrowLeft => 'assets/icons/arrow left.svg';
+
+  /// File path: assets/icons/home-hashtag.svg
+  String get homeHashtag => 'assets/icons/home-hashtag.svg';
+
+  /// File path: assets/icons/list.svg
+  String get list => 'assets/icons/list.svg';
+
+  /// File path: assets/icons/shopping bascket.svg
+  String get shoppingBascket => 'assets/icons/shopping bascket.svg';
+
+  /// File path: assets/icons/user.svg
+  String get user => 'assets/icons/user.svg';
+
+  /// List of all assets
+  List<String> get values => [
+    arrowLeft,
+    homeHashtag,
+    list,
+    shoppingBascket,
+    user,
+  ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/user pic.svg
+  String get userPic => 'assets/images/user pic.svg';
+
+  /// File path: assets/images/whatch store.svg
+  String get whatchStore => 'assets/images/whatch store.svg';
+
+  /// List of all assets
+  List<String> get values => [userPic, whatchStore];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
